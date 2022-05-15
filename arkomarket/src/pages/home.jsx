8b59875a60ab3e4f8b5a7d1e944628484cf09f58
AllenDev1 <dev.allen252@gmail.com>
2022-05-15 23:34:37 +0545

    navbar and footer

--- arkomarket/src/pages/home.jsx
+++ arkomarket/src/pages/home.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import "../css/style.css"
+import "../css/style.css";
 import Filter from "../component/filter";
 
 import { Card, CardGroup, Button, Container, Row } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "../component/nav";
+import Footer from "../component/footer";
 
 const Home = () => {
   return (
     <>
       <NavBar />
-      <div className="mainBody">
-      <Filter />
+      <div className="mainBody" style={{marginTop:'39.5px'}}>
+        <Filter />
+        <Footer/>
       </div>
     
     </>
