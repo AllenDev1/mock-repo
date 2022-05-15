3e206b6acdb7c1b48c828a0ed3d1a868665c763b
Rupesh Khadka <rupesh@alwazebrand.com>
2022-05-15 16:30:50 +0545

    Filter Component

--- arkomarket/src/component/filter.jsx
+++ arkomarket/src/component/filter.jsx
@@ -1,12 +1,136 @@
 import React from 'react'
-import "..css/filter.css"
+import "../css/filter.css"
 
 const Filter = () => {
   return (
     <div className="filter-container">
         <div className="filter-display">
-        <div className="clear"></div>
-        <div className="filter-boxes"></div>
+         <div className="filter-carry" >
+             <div className="filter-flex"><p>Filter</p></div>
+                         
+        <div className="clear"><p>clear</p></div>
+        </div>  
+        <div className="filter-boxes">
+        <select
+              className="Condition"
+              name="find"
+             
+            >
+              <option value selected>
+                Condition
+              </option>
+              <option value="Instagram">Instagram</option>
+              <option value="FaceBook">FaceBook</option>
+              <option value="LinkedIn">LinkedIn</option>
+              <option value="Google">Google</option>
+              <option value="Articles">Articles</option>
+              <option value="Word of Mouth">Word of Mouth</option>
+              <option value="Others">Others</option>
+            </select>
+            <select
+              className="Gurantee"
+              name="find"
+             
+            >
+              <option value selected>
+              Gurantee
+              </option>
+              <option value="Instagram">Instagram</option>
+              <option value="FaceBook">FaceBook</option>
+              <option value="LinkedIn">LinkedIn</option>
+              <option value="Google">Google</option>
+              <option value="Articles">Articles</option>
+              <option value="Word of Mouth">Word of Mouth</option>
+              <option value="Others">Others</option>
+            </select>
+            <select
+              className="State"
+              name="find"
+             
+            >
+              <option value selected>
+              State
+              </option>
+              <option value="Instagram">Instagram</option>
+              <option value="FaceBook">FaceBook</option>
+              <option value="LinkedIn">LinkedIn</option>
+              <option value="Google">Google</option>
+              <option value="Articles">Articles</option>
+              <option value="Word of Mouth">Word of Mouth</option>
+              <option value="Others">Others</option>
+            </select>
+            
+
+            <input
+              className="city"
+              type="text"
+              name="city"
+              placeholder="City"
+            />
+             <input
+              
+              className="Price-from"
+              type="text"
+              name=""
+              placeholder="Price From"
+            />
+           
+            <select
+              className="Year-from"
+              name="from"
+             
+            >
+              <option value selected>
+              Year-from
+              </option>
+              <option value="Instagram">Instagram</option>
+              <option value="FaceBook">FaceBook</option>
+              <option value="LinkedIn">LinkedIn</option>
+              <option value="Google">Google</option>
+              <option value="Articles">Articles</option>
+              <option value="Word of Mouth">Word of Mouth</option>
+              <option value="Others">Others</option>
+            </select>
+            <select
+              className="Year-to"
+              name="to"
+             
+            >
+              <option value selected>
+              Year To
+              </option>
+              <option value="Instagram">Instagram</option>
+              <option value="FaceBook">FaceBook</option>
+              <option value="LinkedIn">LinkedIn</option>
+              <option value="Google">Google</option>
+              <option value="Articles">Articles</option>
+              <option value="Word of Mouth">Word of Mouth</option>
+              <option value="Others">Others</option>
+            </select>
+            
+
+            <input
+              className="price-to"
+              type="text"
+              name="Price To"
+              placeholder="Price To"
+            />
+             <input
+          
+              className="fd"
+              type="text"
+              name="LName"
+              placeholder="Last Name"
+            />
+             <input
+            
+              className="fd"
+              type="text"
+              name="LName"
+              placeholder="Last Name"
+            />
+
+        </div>
         <button className="filter-search">Search</button>
     </div>
     </div>
--- arkomarket/src/pages/home.jsx
+++ arkomarket/src/pages/home.jsx
@@ -1,67 +1,10 @@
 import React from "react";
-import { Card, CardGroup,Button  } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Filter from "../component/filter";
 
 const Home = () => {
   return (
     <>
-      <div className="row my-5" style={{maxWidth: '1090px'}}>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="https://picsum.photos/200/300" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="https://picsum.photos/200/300" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="https://picsum.photos/200/300" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="https://picsum.photos/200/300" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="https://picsum.photos/200/300" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      </div>
+     <Filter/>
     </>
   );
 };
