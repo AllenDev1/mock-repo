5364f5efbfbcd43e8cd971393b2d939521456e98
Rupesh Khadka <rupesh@alwazebrand.com>
2022-05-18 16:27:45 +0545

    Message links

--- arkomarket/src/component/Appformessage.jsx
+++ arkomarket/src/component/Appformessage.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React from "react";
 import "../scss/linkappformessage.scss";
+import Whatsapp from "../Asset/whatsapp.svg";
+import viber from "../Asset/viber.svg";
 const Appformessage = () => {
   return (
     <>
-    <div className="link-app-for-message">
+      <div className="link-app-for-message">
+        <div className="link-message">
+          <h5>Link App for Messages</h5>
+<div className="link-viber-whatsapp">
+          <div className="Whatsapp-link">
+            <div className="whatsapp-contain">
+              <img src={Whatsapp} />
+              <p>Whatsapp</p>
+            </div>
+            <div className="whatsapp-button">
+              <button>Link</button>
+            </div>
+          </div>
 
-    </div>
+          <div className="viber-link">
+            <div className="viber-contain">
+              <img src={viber} />
+              <p>Viber</p>
+            </div>
+            <div className="viber-button">
+              <button>Link</button>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Appformessage;
