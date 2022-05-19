760ad907f6c439816e1f288c0df1288f0c619f49
Rupesh Khadka <rupesh@alwazebrand.com>
2022-05-19 10:52:33 +0545

    notification

--- arkomarket/src/component/Usernotification.jsx
+++ arkomarket/src/component/Usernotification.jsx
@@ -1,11 +1,44 @@
 import React from 'react'
 import "../scss/usernotification.scss";
+import Toggle from "./toggle";
+
+
 const Usernotification = () => {
   return (
     <>
     <div className="user-notification-tab">
+      <h5>Notifications</h5>
+    <div classname="notification-container">
+      <div className="notification-update">
+      <div className="notification-update-Head">
+        <div className="notification-update-details">
+          <h5>gvdchjb</h5>
+          <p>hjdwmns</p>
+        </div>
+        <div className="notification-update-toggel">
+          <Toggle/>
+        </div>
+
+      </div>
+      
+    
+
+      </div>
+    
+    <div className="notification-recomm">
+    <div className="notification-update-recommendations">
+          <h5>gvdchjb</h5>
+          <p>hjdwmns</p>
+        </div>
+        <div className="notification-recommendation-toggel">
+        <Toggle/>
+       
+        </div>
 
     </div>
+    </div>
+    </div>
+    
     </>
   )
 }
