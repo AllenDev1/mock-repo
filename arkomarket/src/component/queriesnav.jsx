51707cea060e9f1c22e3901e0979478f76ac4632
Rupesh Khadka <rupesh@alwazebrand.com>
2022-05-24 16:51:07 +0545

    queries update

--- arkomarket/src/component/queriesnav.jsx
+++ arkomarket/src/component/queriesnav.jsx
@@ -10,28 +10,28 @@ const Queriesnav = () => {
           <p>What does verified user mean?</p>
         </div>
         <div className="queries-section">
-          <p>What does verified user mean?</p>
+          <p>Why should I trust a verified user/seller?</p>
         </div>
         <div className="queries-section">
-          <p>What does verified user mean?</p>
+          <p>How do I search for a product?</p>
         </div>
         <div className="queries-section">
-          <p>What does verified user mean?</p>
+          <p>How can I contact a Seller?</p>
         </div>
         <div className="queries-section">
-          <p>What does verified user mean?</p>
+          <p>Points to remember while checking a product?</p>
         </div>
         <div className="queries-section">
-          <p>What does verified user mean?</p>
+          <p>Can I pay advance to seller?</p>
         </div>
         <div className="queries-section">
-          <p>What does verified user mean?</p>
+          <p>Where should I meet the seller?</p>
         </div>
         <div className="queries-section">
-          <p>What does verified user mean?</p>
+          <p>How can I find Ads that I have seen before?</p>
         </div>
         <div className="queries-section">
-          <p>What does verified user mean?</p>
+          <p>How can I find Ads that I have seen before?</p>
         </div>
         <div className="write-section">
           <p>Can't find what you're looking for?</p>
