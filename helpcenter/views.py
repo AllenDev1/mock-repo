82661a818c8312e358f56afdd4ba3b9ecd35fdfb
bibek <bibektajpuriya.878@gmail.com>
2022-06-28 17:48:02 +0545

    some validations

--- helpcenter/views.py
+++ helpcenter/views.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets
 from .models import HelpCenter
 from .serializer import HelpCenterSerializer
+from rest_framework.permissions import AllowAny
 
 
 class HelpCenterViewSet(viewsets.ModelViewSet):
     queryset = HelpCenter.objects.all()
     serializer_class = HelpCenterSerializer
+    permission_classes = (AllowAny,)
+    http_method_names = ['post']
