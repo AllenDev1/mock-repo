820c590274291992a181b13f92489cc77de30c10
AllenDev1 <dev.allen252@gmail.com>
2022-06-29 14:15:30 +0545

    backend updated

--- faq/views.py
+++ faq/views.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets
 from .serializer import FaqSerializer
 from .models import Faq
+from rest_framework.permissions import AllowAny
 
 
 class FaqViewSet(viewsets.ModelViewSet):
     queryset = Faq.objects.all()
     serializer_class = FaqSerializer
+    permission_classes = (AllowAny,)
+    http_method_names = ['get']
