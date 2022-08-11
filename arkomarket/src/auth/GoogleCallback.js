b8303c430cae2584f26035a51b401cc762de6695
AllenDev1 <dev.allen252@gmail.com>
2022-08-11 17:02:51 +0545

    login

--- arkomarket/src/auth/GoogleCallback.js
+++ arkomarket/src/auth/GoogleCallback.js
@@ -1,12 +1,17 @@
-import React, { useEffect } from "react";
-import { useSearchParams, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { setToken } from "../Redux/authSlice";
 
 const GoogleCallback = () => {
 	const [searchParams, setSearchParams] = useSearchParams();
 
+	const dispatch = useDispatch();
+
 	let navigate = useNavigate();
 	useEffect(() => {
 		localStorage.setItem("access_token", searchParams.get("access_token"));
+		dispatch(setToken(searchParams.get("access_token")));
 		navigate("/");
 	}, [searchParams]);
 
