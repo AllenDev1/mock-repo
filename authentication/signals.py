09d308fc1eacc32c189bb133b4d5fde14a173a8e
bibek <bibektajpuriya.878@gmail.com>
2022-08-18 21:19:39 +0545

    some updates

--- authentication/signals.py
+++ authentication/signals.py
@@ -2,18 +2,52 @@ import hashlib
 from django.dispatch import receiver
 from .models import CustomUser
 
-from allauth.account.signals import user_signed_up, user_logged_in
+from allauth.account.signals import user_logged_in
 
 
-@receiver(user_signed_up)
-def social_login_fname_lname_profilepic(sociallogin, CustomUser):
-    preferred_avatar_size_pixels = 256
+from django.contrib.auth.signals import  user_logged_out, user_login_failed
 
-    social_pic = "http://www.gravatar.com/avatar/{0}?s={1}".format(
-        hashlib.md5(CustomUser.email.encode('UTF-8')).hexdigest(),
-        preferred_avatar_size_pixels
-    )
 
+# @receiver (user_logged_in)
+# def social_login_fname_lname_profilepic(sociallogin,sender, request, user, CustomUser, **kwargs):
+#     print ('into signal')
+#     User = request.user
+#     User.login_status = False
+#     User.save()
+#     # logeduser = CustomUser.objects.filter(id=User.id)
+#     # logeduser.login_status = False
+#     # logeduser.save()
+
+# @receiver (user_signed_up)
+# def social_login_fname_lname_profilepic(sociallogin,sender, request, user, CustomUser, **kwargs):
+#     print ('into signal')
+#     User = request.user
+#     logeduser = CustomUser.objects.filter(id=User.id)
+#     logeduser.login_status = False
+#     logeduser.save()
+
+# @receiver (user_logged_out)
+# def log_user_logout (sender, request, user, **kwargs):
+#     user.login_status = False
+#     user.save()
+
+
+
+@receiver(user_logged_in)
+def social_login(sociallogin, request, user, **kwags):
+    # preferred_avatar_size_pixels = 256
+    
+    # social_pic = "http://www.gravatar.com/avatar/{0}?s={1}".format(
+    #     hashlib.md5(CustomUser.email.encode('UTF-8')).hexdigest(),
+    #     preferred_avatar_size_pixels
+    # )
+    print("testing....")
+    user = request.user
+    customuser = CustomUser.objects.filter(id=user.id)
+    customuser.username = "test1"
+    customuser.save()
+
+    print("testing....")
     if sociallogin:
         # Extract first / last names from social nets and store on User record
         # if sociallogin.account.provider == 'twitter':
@@ -22,27 +56,28 @@ def social_login_fname_lname_profilepic(sociallogin, CustomUser):
         #     user.last_name = name.split()[1]
         print("Social login signal activated !!!")
 
-        if sociallogin.account.provider == 'facebook':
-            f_name = sociallogin.account.extra_data['first_name']
-            l_name = sociallogin.account.extra_data['last_name']
-            if f_name:
-                CustomUser.first_name = f_name
-            if l_name:
-                CustomUser.last_name = l_name
+        # if sociallogin.account.provider == 'facebook':
+        #     f_name = sociallogin.account.extra_data['first_name']
+        #     l_name = sociallogin.account.extra_data['last_name']
+        #     if f_name:
+        #         CustomUser.first_name = f_name
+        #     if l_name:
+        #         CustomUser.last_name = l_name
 
-            # verified = sociallogin.account.extra_data['verified']
-            social_pic = "http://graph.facebook.com/{0}/picture?width={1}&height={1}".format(
-                sociallogin.account.uid, preferred_avatar_size_pixels)
+        #     # verified = sociallogin.account.extra_data['verified']
+        #     social_pic = "http://graph.facebook.com/{0}/picture?width={1}&height={1}".format(
+        #         sociallogin.account.uid, preferred_avatar_size_pixels)
 
         if sociallogin.account.provider == 'google':
-            f_name = sociallogin.account.extra_data['given_name']
-            l_name = sociallogin.account.extra_data['family_name']
-            if f_name:
-                CustomUser.first_name = f_name
-            if l_name:
-                CustomUser.last_name = l_name
-            email_verified = sociallogin.account.extra_data['verified_email']
-            social_pic = sociallogin.account.extra_data['picture']
+            CustomUser.login_status = False
+            # f_name = sociallogin.account.extra_data['given_name']
+            # l_name = sociallogin.account.extra_data['family_name']
+            # if f_name:
+            #     CustomUser.first_name = f_name
+            # if l_name:
+            #     CustomUser.last_name = l_name
+            # email_verified = sociallogin.account.extra_data['verified_email']
+            # social_pic = sociallogin.account.extra_data['picture']
 
-    CustomUser.image = social_pic
+    # CustomUser.image = social_pic
     CustomUser.save()
