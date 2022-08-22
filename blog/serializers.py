4f94e8258ae690883e5371fb16308bdc9d93655d
bibek <bibektajpuriya.878@gmail.com>
2022-08-22 18:45:32 +0545

    blog category name instead of id

--- blog/serializers.py
+++ blog/serializers.py
@@ -2,10 +2,11 @@ from rest_framework import serializers
 from .models import *
 
 class Blog_serializer(serializers.ModelSerializer):
+    blog_categories = serializers.StringRelatedField(source='blog_category', many = True)
     created_by = serializers.StringRelatedField(source='created_by.email')
     class Meta:
         model = Blog
-        fields = ['title', 'body', 'blog_image', 'created_by', 'created_at']
+        fields = ['title', 'body','blog_categories', 'blog_image', 'created_by', 'created_at']
 
 class Blog_Helpful(serializers.ModelSerializer):
     blog = serializers.StringRelatedField(source='blog.title')
