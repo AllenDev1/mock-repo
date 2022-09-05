057e64ad0ab8da357906f6622911fb775b9c2d1e
bibek <bibektajpuriya.878@gmail.com>
2022-09-05 11:04:58 +0545

    carrer and vacancies api error solved

--- carrer/serializer.py
+++ carrer/serializer.py
@@ -6,12 +6,13 @@ from .models import *
 class CarrerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Carrer
-        field = ['title', 'job_type', 'no_of_roles', 'location', 'description']
+        fields = ['title', 'job_type', 'no_of_roles', 'location', 'description']
+       
 
 
 
 class ApplicantSerializer(serializers.ModelSerializer):
     class Meta:
         model = JobApplicants
-        field = ['first_name', 'last_name', 'email', 'phone_number', 'address', 'resume', 'linkedin']
-        
+        fields = ['first_name', 'last_name', 'email', 'phone_number', 'address', 'resume', 'linkedin']
+       
